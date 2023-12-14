8885f01a58db9a9493c459f0abf24f218ce12b07
savananhlin <yahsuanlin@MacBook-Pro-2.local>
2023-12-14 17:12:17 +0800

    update

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://admin:xxxxxxx@cluster0.cthiqik.mongodb.net/acme?retryWrites=true&w=majority"
+const uri = "mongodb+srv://<username>:xxxxxxx@cluster0.cthiqik.mongodb.net/[yourtablename]?retryWrites=true&w=majority"
 
 const client = new MongoClient(uri);
 
